2363db43c5bd8bf02b0967f060f81e3a15df4c6f
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-01-25 21:55:04 +0500

    fix: email validation

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), createUser);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
--- routes/cards.js
+++ routes/cards.js
@@ -9,10 +9,15 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required(),
   }),
 }), createCard);
 
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', celebrate({
+  body: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+}), deleteCard);
 
 router.put('/:cardId/likes', likeCard);
 
