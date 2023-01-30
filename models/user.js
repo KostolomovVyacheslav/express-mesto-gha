1244851102a7ea6aacb66a8f8e3eaad74e68ada8
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-01-30 20:08:11 +0500

    feat: new regex for avatar validation

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(v) {
-        return /^(https|http|www.)?:\/\//im.test(v);
+        return /(https|http|www\.)?:\/\/([\d\w.\-_~:/?[\]@!$&'()*+,;=#]+)(\/)?(#)?/im.test(v);
       },
       message: (props) => `${props.value} это не корректный URL адрес`,
     },
